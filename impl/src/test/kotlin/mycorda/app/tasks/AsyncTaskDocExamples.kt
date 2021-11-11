7e402d21d26eb82e1633f1c63953b814f09895cc
Ian Morgan <ian.j.morgan@gmail.com>
2021-11-11 14:41:46 +0000

    increasing test delay for reliability

--- impl/src/test/kotlin/mycorda/app/tasks/AsyncTaskDocExamples.kt
+++ impl/src/test/kotlin/mycorda/app/tasks/AsyncTaskDocExamples.kt
@@ -45,7 +45,7 @@ class AsyncTaskDocExamples {
         // not yet read
         assertThat(query.hasResult(channelId), equalTo(false))
         // wait long enough
-        Thread.sleep(PlatformTimer.clockTick() * 3)
+        Thread.sleep(PlatformTimer.clockTick() * 3 + 10)
         // now ready
         assert(query.hasResult(channelId))
         val result = query.result<Int>(channelId)
@@ -104,7 +104,7 @@ class AsyncTaskDocExamples {
         // not yet read
         assertThat(query.hasResult(channelId), equalTo(false))
         // wait long enough
-        Thread.sleep(PlatformTimer.clockTick() * 3)
+        Thread.sleep(PlatformTimer.clockTick() * 3 +10)
         // now ready
         assert(query.hasResult(channelId))
         val result = query.result<Int>(channelId)
